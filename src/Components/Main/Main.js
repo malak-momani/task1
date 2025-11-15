c9eb270d9b3fc2578ba731116922b2dd479b515a
malak-momani <malak.momani.bm@gmail.com>
2025-11-15 15:26:20 +0300

    enhance theme

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,14 +3,17 @@ import AddTask from "../Tasks/AddTask";
 import TaskList from "../Tasks/TasksList";
 import { TasksContext } from "../../Contexts/TasksContext";
 import ToggleTheme from "../ThemeToggle/ThemeToggle";
+import "./Main.css"
 
 const Main = () => {
     const { tasks } = useContext(TasksContext)
     console.log(tasks)
     return (
         <div>
-            <ToggleTheme />
-            <AddTask />
+            <div className="first-container">
+                <ToggleTheme />
+                <AddTask />
+            </div>
             <TaskList tasks={tasks} />
         </div>
     )
